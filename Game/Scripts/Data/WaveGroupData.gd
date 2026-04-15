5637db982dbdd8fd641646fca81e60241b6c08b5
FallenBuilder <FallenBuilder@outlook.com>
2026-04-15 23:38:43 +0200

    Delete Game directory

--- Game/Scripts/Data/WaveGroupData.gd
+++
@@ -1,9 +0,0 @@
-class_name WaveGroupData
-extends Resource
-
-@export var enemy_id: int = 1
-@export var count: int = 1
-@export var start: float = 0.0
-@export var end: float = 5.0
-@export var camo: bool = false
-@export var regen: bool = false
