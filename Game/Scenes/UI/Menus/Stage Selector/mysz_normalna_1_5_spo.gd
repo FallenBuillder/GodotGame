489a70df94e6d8d519c046cb890eac140dc183ce
FallenBuilder <FallenBuilder@outlook.com>
2026-04-22 19:20:06 +0200

    removed stuff

--- Game/Scenes/UI/Menus/Stage Selector/mysz_normalna_1_5_spo.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-# Prędkość obrotu (w radianach na sekundę)
-var rotation_speed = 2.0 
-
-func _process(delta):
-	# Obracamy o prędkość pomnożoną przez czas klatki
-	rotation += rotation_speed * delta
--- Game/Scripts/Data/StatChange.gd
+++
@@ -1,9 +0,0 @@
-class_name StatChange
-extends Resource
-
-enum Mode { ADD, SET, MULTIPLY }
-
-@export var stat: String = ""
-@export var mode: Mode = Mode.ADD
-@export var value: float = 0.0
-@export var scene_value: PackedScene
